7cfc284d97f389efd3ebaa51d78f5a60b964634e
Timo Jääskeläinen <timo.jaaskelainen@reaktor.com>
2017-07-15 16:09:13 -0400

    Extract FakeRemoteService out of RemoteDataService for clarity

--- ReSwiftAsyncMiddlewarePattern/Services/RemoteDataService.swift
+++ ReSwiftAsyncMiddlewarePattern/Services/RemoteDataService.swift
@@ -1,47 +1,49 @@
 import PromiseKit
 
 struct RemoteDataService: DataService {
-    private static var posts = [
-        Post(title: "Post One", body: "Body of post one."),
-        Post(title: "Post Two", body: "Body of post two."),
-        Post(title: "Post Three", body: "Body of post three."),
-    ]
-
-    private static let users = [
-        User(firstName: "User", lastName: "One"),
-        User(firstName: "User", lastName: "Two"),
-        User(firstName: "User", lastName: "Three"),
-    ]
-
-    private static func executeWithDelay(closure: @escaping () -> ()) {
-        let artificialDelaySeconds = 2.0
-        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + artificialDelaySeconds) {
-            closure()
-        }
-    }
-
     func fetchUsers() -> Promise<[User]> {
         return Promise<[User]> { fulfill, reject in
-            RemoteDataService.executeWithDelay {
-                fulfill(RemoteDataService.users)
+            FakeRemoteService.executeWithDelay {
+                fulfill(FakeRemoteService.users)
             }
         }
     }
 
     func fetchPosts() -> Promise<[Post]> {
         return Promise<[Post]> { fulfill, reject in
-            RemoteDataService.executeWithDelay {
-                fulfill(RemoteDataService.posts)
+            FakeRemoteService.executeWithDelay {
+                fulfill(FakeRemoteService.posts)
             }
         }
     }
 
     func createPost(post: Post) -> Promise<Void> {
         return Promise<Void> { fulfill, reject in
-            RemoteDataService.executeWithDelay {
-                RemoteDataService.posts.append(post)
+            FakeRemoteService.executeWithDelay {
+                FakeRemoteService.posts.append(post)
                 fulfill()
             }
+        }
+    }
+}
+
+fileprivate struct FakeRemoteService {
+    static var posts = [
+        Post(title: "Post One", body: "Body of post one."),
+        Post(title: "Post Two", body: "Body of post two."),
+        Post(title: "Post Three", body: "Body of post three."),
+        ]
+
+    static let users = [
+        User(firstName: "User", lastName: "One"),
+        User(firstName: "User", lastName: "Two"),
+        User(firstName: "User", lastName: "Three"),
+        ]
+
+    static func executeWithDelay(closure: @escaping () -> ()) {
+        let artificialDelaySeconds = 2.0
+        DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + artificialDelaySeconds) {
+            closure()
         }
     }
 }
