ab0bd0a49f927b35d4c5950c7c16c8d29caa9c42
Timo Jääskeläinen <timo.jaaskelainen@reaktor.com>
2017-07-17 20:03:52 -0400

    Add fetchUsers side effect test

---
+++ ReSwiftAsyncMiddlewarePatternTests/FetchUsersSpec.swift
@@ -0,0 +1,60 @@
+import Quick
+import Nimble
+
+class FetchUsersSpec: QuickSpec {
+
+    override func spec() {
+        describe("fetchUsers") {
+
+            it("Does not call fetchUsers service when action is not FetchUsers") {
+                let testDataService = TestDataService(posts: [], users: [], shouldFail: false) {
+                    fail("The test was not supposed to call fetchUsers DataService method")
+                }
+                let middlewareItem = fetchUsers(dataService: testDataService)
+                middlewareItem(FetchPosts.request) { _ in }
+            }
+
+            it("Does not call fetchUsers service when action is FetchUsers and type is not request") {
+                let testDataService = TestDataService(posts: [], users: [], shouldFail: false) {
+                    fail("The test was not supposed to call fetchUsers DataService method")
+                }
+                let middlewareItem = fetchUsers(dataService: testDataService)
+                middlewareItem(FetchUsers.success(users: [])) { _ in }
+            }
+
+            it("Dispatches FetchUsers.success action with correct users after FetchUsers.request is passed") {
+                let expected = randomUsers()
+                let testDataService = TestDataService(posts: [], users: expected, shouldFail: false, fetchUsersCallback: nil)
+                let middlewareItem = fetchUsers(dataService: testDataService)
+
+                var actual: [User] = []
+                middlewareItem(FetchUsers.request) { action in
+                    guard let action = action as? FetchUsers,
+                        case .success(let fetchedUsers) = action else {
+                            fail("Expected middleware to dispatch FetchUsers.success action")
+                            return
+                    }
+                    actual = fetchedUsers
+                }
+                expect(actual).toEventually(equal(expected), timeout: 1)
+            }
+
+            it("Dispatches FetchUsers.error action if error occurs") {
+                let testDataService = TestDataService(posts: [], users: [], shouldFail: true, fetchUsersCallback: nil)
+                let middlewareItem = fetchUsers(dataService: testDataService)
+
+                let expected = TestDataServiceError.someError
+                var actual: Error?
+                middlewareItem(FetchUsers.request) { action in
+                    guard let action = action as? FetchUsers,
+                        case .failure(let error) = action else {
+                            fail("Expected middleware to dispatch FetchUsers.failure action")
+                            return
+                    }
+                    actual = error
+                }
+                expect(actual).toEventually(matchError(expected), timeout: 1)
+            }
+        }
+    }
+}
---
+++ ReSwiftAsyncMiddlewarePatternTests/Utils/TestDataService.swift
@@ -0,0 +1,37 @@
+import PromiseKit
+
+enum TestDataServiceError: Error {
+    case someError
+}
+
+struct TestDataService: DataService {
+    let posts: [Post]
+    let users: [User]
+    let shouldFail: Bool
+    let fetchUsersCallback: (() -> Void)?
+
+    func fetchPosts() -> Promise<[Post]> {
+        return promise(shouldFail: self.shouldFail) { $0(self.posts) }
+    }
+
+    func fetchUsers() -> Promise<[User]> {
+        if let fetchUsersCallback = self.fetchUsersCallback {
+            fetchUsersCallback()
+        }
+        return promise(shouldFail: self.shouldFail) { $0(self.users) }
+    }
+
+    func createPost(post: Post) -> Promise<Void> {
+        return promise(shouldFail: self.shouldFail) { $0() }
+    }
+
+    private func promise<T>(shouldFail: Bool, _ fulfillCall: (_ fulfill: (T) -> Void) -> Void) -> Promise<T> {
+        return Promise { (fulfill, reject) in
+            guard !shouldFail else {
+                reject(TestDataServiceError.someError)
+                return
+            }
+            fulfillCall(fulfill)
+        }
+    }
+}
